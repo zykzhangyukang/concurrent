7355c688581a3c7a7f25a664620393b05ea15f7c
zykzhangyukang <3053161401@qq.com>
2021-01-18 11:51:46 +0800

    init springboot project

--- src/main/java/com/coderman/concurrent/ConcurrentApplication.java
+++ src/main/java/com/coderman/concurrent/ConcurrentApplication.java
@@ -1,9 +1,16 @@
 package com.coderman.concurrent;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author zhangyukang
  * @Date 2021/1/18 11:42
  * @Version 1.0
  **/
+@SpringBootApplication
 public class ConcurrentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentApplication.class,args);
+    }
 }
